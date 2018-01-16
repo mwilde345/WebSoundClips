5c878b64f203c0fef5f1dd99067f5b685cbd8f7a
Wilde <wildmat@186590d2c0b7.ant.amazon.com>
2018-01-16 13:44:35 -0800

    update server file

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.listen('3000');
 //ffmpeg -i <input-file> -ac 2 -codec:a libmp3lame -b:a 48k -ar 16000 <output-file.mp3>
 //ffmpeg -y -i <input-file> -ss 00:00:35(or 35) -to 00:00:41 -c copy <output-file>
 
+app.io.route('loud', function(req){
+
+});
+
 app.io.route('trim', function (req){
     //don't let them trim on the client side unless there are actually segments in peaks.
     var input = req.data;
@@ -19,13 +23,12 @@ app.io.route('trim', function (req){
     var end = input.end;
     var videoID = input.videoID;
     var clipID = videoID+"_"+start+"_"+end;
-    var filename = clipID+".mp3";
-    trimClip(req,filename);
+    trimClip(req,videoID, clipID);
 });
 
-function trimClip(req, filename){
+function trimClip(req, videoID, clipID){
     const { exec } = require('child_process');
-    exec('ffmpeg -y -i '+videoID+'.mp3 -ss '+start+' -to '+end+' -c copy '+filename, 
+    exec('ffmpeg -y -i '+videoID+'.mp3 -ss '+start+' -to '+end+' -c copy '+clipID+'.mp3', 
         (err, stdout, stderr) => {
             if (err) {
                 // node couldn't execute the command
@@ -35,7 +38,7 @@ function trimClip(req, filename){
             }
             // the *entire* stdout and stderr (buffered)
             //console.log(`stdout: ${stdout}`);
-            req.io.emit("trim_done","Completed: "+clipID);
+            req.io.emit("trim_done",{clipID: clipID, loudness: -14});
             //console.log(`stderr: ${stderr}`);
         });
 }
@@ -58,6 +61,7 @@ app.io.route('download', function (req) {
             //console.log(result);
             //res.send(result);
             if (result === horizon.successType.CONVERSION_FILE_COMPLETE) {
+                console.log('emitting finished');
                 req.io.emit('download_finished',videoID);
                 //console.log(result);
                 //res.send(JSON.stringify({videoID: videoID}));
